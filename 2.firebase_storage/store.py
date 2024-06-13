dcd19f5a01b580f65499753942d1b9aeed004330
Ashwin.B <ahnashwin1305@gmail.com>
2024-06-13 17:44:13 +0530

    Update store.py

--- 2.firebase_storage/store.py
+++ 2.firebase_storage/store.py
@@ -13,14 +13,14 @@ firebase = pyrebase.initialize_app(firebaseConfig)
 storage = firebase.storage()
 
 # Store data
-# filename = input("Enter name of file that you want to store: ")
-# cloudfilename = input("Enter name for file on cloud: ")
-# storage.child("data/textfiles/"+cloudfilename).put(filename)
+filename = input("Enter name of file that you want to store: ")
+cloudfilename = input("Enter name for file on cloud: ")
+storage.child("data/textfiles/"+cloudfilename).put(filename)
 
 # Retrive data
 get_filename = input("Enter name of file that you want to fetch: ")
 print(storage.child("data/textfiles/"+get_filename).get_url(None))
 
-Download Data
+#Download Data
 down_filename = input("Enter name of file that you want to download: ")
 storage.child("data/textfiles/"+down_filename).download("","cloud.txt")
